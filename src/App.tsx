1e6b80423a281a7fee2317124dfbe7bbdbde4653
Nikita <nikitalochmatov@gmail.com>
2023-09-15 21:15:15 +0300

    update

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Lab1 from './labs/Lab1';
+
+
+export default function App() {
+  return (
+    <>
+      <div className='header'>
+        <h1>Лабораторные работы по компьютерной графике</h1>
+        <h2>Выполнил Лохматов Никита</h2>
+      </div>
+      <Lab1 />
+    </>
+  );
+}
---
+++ src/labs/Lab1.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default function Lab1() {
+	return (
+		<div className='labBlock'>
+			<h1>Lab 1</h1>
+		</div>
+	);
+}
